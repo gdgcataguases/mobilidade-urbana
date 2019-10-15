e8466918a96d65509f7fb830433196e7a680bff3
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-15 20:26:09 -0300

    Victor Emidio

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -63,28 +63,34 @@ class SearchBar extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(50, 30, 50, 20),
-        child: Column(
+        child: Row(
           children: <Widget>[
-            Center(
-              child: Text(
-                'KTAMOBI',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 60,
-                    fontWeight: FontWeight.bold),
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      'KTAMOBI',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            labelText: "Pesquise aqui",
+                            labelStyle: TextStyle(color: Colors.white),
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)))),
+                  )
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      labelText: "Pesquise aqui",
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white)))),
-            )
           ],
         ),
       ),
