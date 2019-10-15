bcfea94a6cc0a57e83d4e41a21e5d023c4ab43c7
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-15 20:20:45 -0300

    Luiz Antônio

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -52,33 +52,40 @@ class SearchBar extends StatelessWidget {
     return SafeArea(
       child: Container(
         color: Colors.red,
-        child: SizedBox(
-          height: 300,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(50, 30, 50, 20),
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    'KTAMOBI',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        labelText: "Pesquise aqui",
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder(),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white))))
-              ],
+        child: getTop(context),
+      ),
+    );
+  }
+
+  SizedBox getTop(BuildContext context) {
+    return SizedBox(
+      height: 240,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 30, 50, 20),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text(
+                'KTAMOBI',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      labelText: "Pesquise aqui",
+                      labelStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)))),
+            )
+          ],
         ),
       ),
     );
