4322533d752f412fd409022e70f189caac4ce4bf
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-15 19:29:38 -0300

    Luiz Antônio

---
+++ lib/ui/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(title: ''),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'KTAMOBI',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
