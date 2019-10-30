1d51d41972e549c4fd23587626f95c36f91042d8
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-29 21:06:02 -0300

    Carlos Sulzer

--- lib/ui/rotas.dart
+++ lib/ui/rotas.dart
@@ -3,28 +3,32 @@ import 'package:flutter/material.dart';
 class RotasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: TabBar(
-          tabs: <Widget>[
-            Tab(
-              text: 'TAB',
-            ),
-            Tab(
-              text: 'TAB 2',
-            )
-          ],
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Tab(
-              text: 'TEXT',
-            ),
-            Tab(
-              text: 'TEXT 2',
-            )
-          ],
+    return Container(
+      color: Colors.red,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Saida',
+              ),
+              Tab(
+                text: 'Saida',
+              )
+            ],
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Tab(
+                text: 'TEXT',
+              ),
+              Tab(
+                text: 'TEXT 2',
+              )
+            ],
+          ),
         ),
       ),
     );
